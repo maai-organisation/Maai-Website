7084056e0c646c83e35a133f872fe55cabbe8114
Sushmit Morey <sushmitmorey1805@gmail.com>
2026-05-22 00:39:25 +0530

    .env fixed --> migrated from local to aivencloud

--- backend/config/db.js
+++ backend/config/db.js
@@ -17,16 +17,11 @@ console.log({
 
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
-  port: Number(process.env.DB_PORT),
+  port: process.env.DB_PORT,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  waitForConnections: true,
-  connectionLimit: Number(process.env.DB_CONNECTION_LIMIT || 10),
-  queueLimit: 0,
-  ssl: {
-    rejectUnauthorized: false,
-  },
+  ssl: { rejectUnauthorized: false }, // ← required for Aiven
 });
 
 if (typeof pool.on === "function") {
